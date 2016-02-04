5a66a6535c8a2458705b8df0d6b1ffd8d25977f8
Akshay Yadav <akshay-yadav@uiowa.edu>
2016-02-03 22:25:12 -0600

    First Commit and project build

---
+++ CMP_0.0/CMP_0.0Tests/CMP_0_0Tests.swift
@@ -0,0 +1,36 @@
+//
+//  CMP_0_0Tests.swift
+//  CMP_0.0Tests
+//
+//  Created by Akshay's on 2/3/16.
+//  Copyright © 2016 Akshay's. All rights reserved.
+//
+
+import XCTest
+@testable import CMP_0_0
+
+class CMP_0_0Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
